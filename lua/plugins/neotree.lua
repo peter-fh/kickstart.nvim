92979fcb46a7e411e355eb4b8ec842f26350b07b
peter-fh <p.felty.howe@gmail.com>
2024-12-12 21:59:37 -0500

    Split large init file into separate files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,3 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -11,7 +8,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
