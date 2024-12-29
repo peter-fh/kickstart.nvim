473794fd1ac22bc70455190de27dbd18d6886cf0
peter-fh <p.felty.howe@gmail.com>
2024-12-28 23:30:50 -0500

    feat(lsp): add more lsp servers by default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -165,19 +165,14 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
+        clangd = {},
+        gopls = {},
+        basedpyright = {},
+        rust_analyzer = {},
         --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        ts_ls = {},
 
+        jdtls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -192,6 +187,7 @@ return {
             },
           },
         },
+        cmake = {},
       }
 
       -- Ensure the servers and tools above are installed
