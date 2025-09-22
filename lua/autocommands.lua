0e8c137a9d6b1530762aed771e0813198f609c6d
peter-fh <p.felty.howe@gmail.com>
2025-09-21 23:27:07 -0400

    fixup: save progress

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -8,3 +8,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'html',
+  callback = function(args)
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.treesitter.start(args.buf, 'gotmpl') -- Attach the 'gotmpl' parser in addition to the default 'html' parser
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescriptreact",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+    -- Optional: vim.bo.textwidth = 80
+  end,
+  desc = "Set indentation for TypeScript React files",
+})
--- lua/plugins/typr.lua
+++ lua/plugins/typr.lua
@@ -1,6 +1,8 @@
 return {
     "nvzone/typr",
     dependencies = "nvzone/volt",
+    dir = "~/dev/typr",
+    dev = true,
     opts = {},
     cmd = { "Typr", "TyprStats" },
 }
